2ea23d00266c3f88a18fd675333fbaf1ab61b44a
Ramonprog <programador.ramon@gmail.com>
2023-07-18 21:59:20 -0300

    layout inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return(
-    <main></main>
+   <div className="h-screen flex flex-col">
+    <div className="flex flex-1">
+      <aside className="w-72 bg-zinc-950 p-6">SideBar</aside>
+      <main className="flex-1 p-6">Main</main>
+    </div>
+    <footer className="bg-zinc-800 border-t border-zinc-700 p-6">Footer</footer>
+   </div>
   )
 }
