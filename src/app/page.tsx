a9c28600b8f7c74c4daf7dfc21be8afb25ce46d6
Ramonprog <programador.ramon@gmail.com>
2023-07-23 20:01:29 -0300

    grid albuns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,13 +89,43 @@ export default function Home() {
               </button>
             </div>
             <div className="bg-white/10 rounded group overflow-hidden flex items-center gap-4 cursor-pointer hover:bg-white/20 transition-colors">
-              <Image src="/alucinacao.jpg" alt="capa album mamonas assassinas" width={100} height={100} />
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" width={100} height={100} />
               <strong>Alucinação</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
             </div>
 
+          </div>
+
+          <h2 className="font-smibold  mt-10">Made for Alisson Ramon</h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <div className="bg-white/5 p-3 cursor-pointer hover:bg-white/10 transition-colors rounded-md flex flex-col gap-2">
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" className="w-full" width={100} height={100} />
+              <strong className="font-semibold ">Alucinação</strong>
+              <span className="text-sm text-zinc-500">Belchior</span>
+            </div>
+            <div className="bg-white/5 p-3 cursor-pointer hover:bg-white/10 transition-colors rounded-md flex flex-col gap-2">
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" className="w-full" width={100} height={100} />
+              <strong className="font-semibold ">Alucinação</strong>
+              <span className="text-sm text-zinc-500">Belchior</span>
+            </div>
+            <div className="bg-white/5 p-3 cursor-pointer hover:bg-white/10 transition-colors rounded-md flex flex-col gap-2">
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" className="w-full" width={100} height={100} />
+              <strong className="font-semibold ">Alucinação</strong>
+              <span className="text-sm text-zinc-500">Belchior</span>
+            </div>
+            <div className="bg-white/5 p-3 cursor-pointer hover:bg-white/10 transition-colors rounded-md flex flex-col gap-2">
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" className="w-full" width={100} height={100} />
+              <strong className="font-semibold ">Alucinação</strong>
+              <span className="text-sm text-zinc-500">Belchior</span>
+            </div>
+            <div className="bg-white/5 p-3 cursor-pointer hover:bg-white/10 transition-colors rounded-md flex flex-col gap-2">
+              <Image src="/alucinacao.jpg" alt="Capa do cd alucinação de Belchior" className="w-full" width={100} height={100} />
+              <strong className="font-semibold ">Alucinação</strong>
+              <span className="text-sm text-zinc-500">Belchior</span>
+            </div>
           </div>
         </main>
       </div>
